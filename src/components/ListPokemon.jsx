7075f706b03f57078e37d72c6ac9afd76ea677d7
tqaw19 <tqaw19@gmail.com>
2021-09-29 01:28:19 -0500

    added responsive grid card list.

--- src/components/ListPokemon.jsx
+++ src/components/ListPokemon.jsx
@@ -1,5 +1,23 @@
+import { Box, Grid, Heading } from "@chakra-ui/layout";
 import React from "react";
 
 export default function ListPokemon() {
-  return <h2>hi</h2>;
+  return (
+    <>
+      <Heading size="lg" textAlign="center" mt="9">
+        Pokemon List
+      </Heading>
+      <Grid
+        templateColumns="repeat(auto-fit, minmax(200px, 1fr))"
+        gap={4}
+        py="16"
+        px={["6", null, null, "28", "44"]}
+      >
+        <Box w="100%" h="10" bg="blue.500" />
+        <Box w="100%" h="10" bg="blue.500" />
+        <Box w="100%" h="10" bg="blue.500" />
+        <Box w="100%" h="10" bg="blue.500" />
+      </Grid>
+    </>
+  );
 }
