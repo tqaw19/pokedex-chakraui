7de0e0a9f10fd04ac45af1ff8ed210b86489569c
tqaw19 <tqaw19@gmail.com>
2021-10-18 15:32:20 -0500

    Added id prop to show pokemon details dynamically right from the list. Added Link component to handle details.

--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -1,48 +1,48 @@
 import { Image } from "@chakra-ui/image";
 import { Box } from "@chakra-ui/layout";
 import React from "react";
+import { Link } from "react-router-dom";
 
 import BadgePokemon from "./BadgePokemon";
 
 export default function CardPokemon(pokemon) {
-  const { name, order, sprites, types } = pokemon;
+  const { name, order, sprites, types, id } = pokemon;
 
   const pokemonImage = sprites.other["official-artwork"]["front_default"];
 
   return (
     // {/**Pokemon Card */}
-    <Box
-      maxW="sm"
-      borderRadius="lg"
-      overflow="hidden"
-      shadow="md"
-      bg="gray.200"
-    >
-      <Image src={pokemonImage} alt={pokemonImage} />
-
-      <Box p="6">
-        {/**Order number */}
-        <Box as="span" color="gray.600" fontSize="sm">
-          #{order}
-        </Box>
-
-        {/**Name */}
-        <Box
-          my="2"
-          fontWeight="semibold"
-          fontSize="sm"
-          as="h4"
-          lineHeight="tight"
-          isTruncated
-        >
-          {name}
-        </Box>
-
-        {/**Pokemon Types goes here */}
-        <Box d="flex" flexDirection="column" alignItems="baseline">
-          <BadgePokemon types={types} />
+    <Link to={`/details/${id}`}>
+      <Box
+        maxW="sm"
+        borderRadius="lg"
+        overflow="hidden"
+        shadow="md"
+        bg="gray.200"
+      >
+        <Image src={pokemonImage} alt={pokemonImage} />
+        <Box p="6">
+          {/**Order number */}
+          <Box as="span" color="gray.600" fontSize="sm">
+            #{order}
+          </Box>
+          {/**Name */}
+          <Box
+            my="2"
+            fontWeight="semibold"
+            fontSize="sm"
+            as="h4"
+            lineHeight="tight"
+            isTruncated
+          >
+            {name}
+          </Box>
+          {/**Pokemon Types goes here */}
+          <Box d="flex" flexDirection="column" alignItems="baseline">
+            <BadgePokemon types={types} />
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </Link>
   );
 }
