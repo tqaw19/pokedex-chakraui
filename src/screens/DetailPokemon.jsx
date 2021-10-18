9ed0fe7f496097d41cdfa820ba02d09abca35372
tqaw19 <tqaw19@gmail.com>
2021-10-18 15:49:06 -0500

    Moved Spinner component so it loads inside the Container to avoid the blinking effect when loading the screen.

--- src/screens/DetailPokemon.jsx
+++ src/screens/DetailPokemon.jsx
@@ -36,9 +36,87 @@ export default function DetailPokemon({ match }) {
   // console.log(pokemonData[1]);
   // console.log(id);
 
-  if (status === "loading") {
-    return <Spinner />;
-  }
+  const PokemonDataRendered = () => (
+    <>
+      {/** Pokemon Name and Order No. */}
+      <Heading size="lg" mt="8" textAlign="center">
+        {name?.toUpperCase()} <Text display="inline">#{order}</Text>
+      </Heading>
+
+      <Flex mt="16" px="44">
+        <Box width="50%" display="flex" justifyContent="center">
+          <Image
+            boxSize="fit-content"
+            src={pokemonImage}
+            alt={name}
+            borderRadius="lg"
+            shadow="md"
+            background="gray.200"
+          />
+        </Box>
+
+        {/** Short Descrioption */}
+        <Box width="50%" p="4">
+          <Text fontSize="sm">{pokemonDescription}</Text>
+
+          {/** Pokemon Attributes */}
+          <Flex
+            bg="blue.400"
+            mt="4"
+            borderRadius="lg"
+            fontSize="sm"
+            color="white"
+          >
+            <Text as="span" m="4">
+              Height{" "}
+              <Text mb="4" color="black">
+                2 04
+              </Text>
+              Weight{" "}
+              <Text mb="4" color="black">
+                200
+              </Text>
+              Gender{" "}
+              <Text mb="4" color="black">
+                Male
+              </Text>
+            </Text>
+            <Spacer />
+            <Text as="span" m="4" mr="16">
+              Category{" "}
+              <Text mb="4" color="black">
+                Seed
+              </Text>
+              Abilities{" "}
+              <Text mb="4" color="black">
+                Overgrow
+              </Text>
+            </Text>
+          </Flex>
+
+          {/** Type */}
+          <Box mt="4">
+            <Text mb="1">Type</Text>
+            <BadgePokemon types={types ?? []} />
+          </Box>
+
+          {/** Weaknesses */}
+          <Box mt="4">
+            <Text>Weaknesses</Text>
+            <Badge borderRadius="full" px="2" py="0.5" mb="1">
+              Ghost
+            </Badge>
+            <Badge borderRadius="full" px="2" py="0.5" mb="1" ml="2">
+              Water
+            </Badge>
+            <Badge borderRadius="full" px="2" py="0.5" mb="1" ml="2">
+              Physic
+            </Badge>
+          </Box>
+        </Box>
+      </Flex>
+    </>
+  );
 
   return (
     <Box
@@ -49,83 +127,7 @@ export default function DetailPokemon({ match }) {
       marginBottom="0"
     >
       <Container bg="brand.800" maxW="container.xl" p="0">
-        {/** Pokemon Name and Order No. */}
-        <Heading size="lg" mt="8" textAlign="center">
-          {name?.toUpperCase()} <Text display="inline">#{order}</Text>
-        </Heading>
-
-        <Flex mt="16" px="44">
-          <Box width="50%" display="flex" justifyContent="center">
-            <Image
-              boxSize="fit-content"
-              src={pokemonImage}
-              alt={name}
-              borderRadius="lg"
-              shadow="md"
-              background="gray.200"
-            />
-          </Box>
-
-          {/** Short Descrioption */}
-          <Box width="50%" p="4">
-            <Text fontSize="sm">{pokemonDescription}</Text>
-
-            {/** Pokemon Attributes */}
-            <Flex
-              bg="blue.400"
-              mt="4"
-              borderRadius="lg"
-              fontSize="sm"
-              color="white"
-            >
-              <Text as="span" m="4">
-                Height{" "}
-                <Text mb="4" color="black">
-                  2 04
-                </Text>
-                Weight{" "}
-                <Text mb="4" color="black">
-                  200
-                </Text>
-                Gender{" "}
-                <Text mb="4" color="black">
-                  Male
-                </Text>
-              </Text>
-              <Spacer />
-              <Text as="span" m="4" mr="16">
-                Category{" "}
-                <Text mb="4" color="black">
-                  Seed
-                </Text>
-                Abilities{" "}
-                <Text mb="4" color="black">
-                  Overgrow
-                </Text>
-              </Text>
-            </Flex>
-
-            {/** Type */}
-            <Box mt="4">
-              <Text mb="1">Type</Text>
-              <BadgePokemon types={types ?? []} />
-            </Box>
-
-            {/** Weaknesses */}
-            <Box mt="4">
-              <Text>Weaknesses</Text>
-              <Badge borderRadius="full" px="2" py="0.5" mb="1">
-                Ghost
-              </Badge>
-              <Badge borderRadius="full" px="2" py="0.5" mb="1" ml="2">
-                Water
-              </Badge>
-              <Badge borderRadius="full" px="2" py="0.5" mb="1" ml="2">
-                Physic
-              </Badge>
-            </Box>
-          </Box>
-        </Flex>
+        {status === "loading" ? <Spinner /> : <PokemonDataRendered />}
       </Container>
     </Box>
   );
