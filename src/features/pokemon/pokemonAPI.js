d1e0482d2bfae495546932983dd00f168dccda94
tqaw19 <tqaw19@gmail.com>
2021-10-23 00:19:04 -0500

    Fetching function now accepts a value number for rendering its next offset.

--- src/features/pokemon/pokemonAPI.js
+++ src/features/pokemon/pokemonAPI.js
@@ -1,9 +1,11 @@
 import { client } from "../../config/axios";
 
 // Function that fetch initial pokemon list from pokeapi
-export async function fetchPokemon() {
+export async function fetchPokemon(offsetValue) {
   try {
-    const endPointList = await client.get(`pokemon?offset=${10}&limit=10`);
+    const endPointList = await client.get(
+      `pokemon?offset=${offsetValue}&limit=10`
+    );
     const endPointResponse = await Promise.all(
       endPointList.data.results.map((link) => client.get(link.url))
     );
