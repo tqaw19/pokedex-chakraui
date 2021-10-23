e7dd63998a89aee47d7d5b822ee4c173020cd553
tqaw19 <tqaw19@gmail.com>
2021-10-23 00:18:02 -0500

    Added button that fetch next offset of pokemons.

--- src/components/ListPokemon.jsx
+++ src/components/ListPokemon.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Grid, Heading } from "@chakra-ui/layout";
+import { Flex, Grid, Heading } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
 
 import CardPokemon from "./CardPokemon";
-import { fetchInitialPokemonList } from "../features/pokemon/pokemonSlice";
+import {
+  fetchInitialPokemonList,
+  nextOffset,
+} from "../features/pokemon/pokemonSlice";
 import SpinnerComponent from "./SpinnerComponent";
 
 export default function ListPokemon() {
@@ -11,32 +15,48 @@ export default function ListPokemon() {
   // const pokemonStatus = useSelector((state) => state.pokemon.status);
   const pokemonData = useSelector((state) => state.pokemon.pokemon);
   const status = useSelector((state) => state.pokemon.status);
+  const offsetValue = useSelector((state) => state.pokemon.offset);
 
   useEffect(() => {
-    if (pokemonData.length === 0) {
-      dispatch(fetchInitialPokemonList());
+    console.log(offsetValue);
+    if (pokemonData.length !== offsetValue) {
+      dispatch(fetchInitialPokemonList(offsetValue));
     }
     // eslint-disable-next-line
-  }, []);
+  }, [offsetValue]);
 
   return (
     <>
       <Heading size="lg" textAlign="center" mt="9">
         Pokemon List
       </Heading>
-      {status === "loading" ? (
+      {status === "loading" && pokemonData.length === 0 ? (
         <SpinnerComponent />
       ) : (
-        <Grid
-          templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
-          gap={4}
-          py="16"
-          px={["6", null, null, "28", "44"]}
-        >
-          {pokemonData?.map((pokemon) => (
-            <CardPokemon key={pokemon.id} {...pokemon} />
-          ))}
-        </Grid>
+        <>
+          <Grid
+            templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+            gap={4}
+            py="16"
+            px={["6", null, null, "28", "44"]}
+          >
+            {pokemonData?.map((pokemon) => (
+              <CardPokemon key={pokemon.id} {...pokemon} />
+            ))}
+          </Grid>
+          <Flex justify="center">
+            <Button
+              onClick={() => dispatch(nextOffset())}
+              colorScheme="brand.200"
+              bg="brand.200"
+              _hover={{ background: "#04aaa5" }}
+              mb="16"
+              mx="auto"
+            >
+              Load More
+            </Button>
+          </Flex>
+        </>
       )}
     </>
   );
