6e23cda4f0a37a100de1aa2a093a394a1c9909f1
tqaw19 <tqaw19@gmail.com>
2021-10-22 22:54:53 -0500

    Created new screen section.

---
+++ src/screens/Categories.jsx
@@ -0,0 +1,10 @@
+import { Text } from "@chakra-ui/layout";
+import React from "react";
+
+export const Categories = () => {
+  return (
+    <div>
+      <Text>This the categories page</Text>
+    </div>
+  );
+};
---
+++ src/screens/MyPokemons.jsx
@@ -0,0 +1,10 @@
+import { Text } from "@chakra-ui/layout";
+import React from "react";
+
+export const MyPokemons = () => {
+  return (
+    <div>
+      <Text>This page will show your pokemons.</Text>
+    </div>
+  );
+};
