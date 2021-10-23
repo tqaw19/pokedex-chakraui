4515e9fe73ba2b20794a8c708f4b22d994d6ccd5
tqaw19 <tqaw19@gmail.com>
2021-10-23 12:34:11 -0500

    Added multiplier relations.

---
+++ src/utils/multiplierRelations.js
@@ -0,0 +1,11 @@
+export const multiplierDamageFrom = {
+  double_damage_from: "x2",
+  half_damage_from: "x0.5",
+  no_damage_from: "x0",
+};
+
+export const multiplierDamageTo = {
+  double_damage_to: "x2",
+  half_damage_to: "x0.5",
+  no_damage_to: "x0",
+};
