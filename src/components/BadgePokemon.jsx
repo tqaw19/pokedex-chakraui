eee640a3f0ccb9417ab1d0c074f54a51d00399f2
tqaw19 <tqaw19@gmail.com>
2021-10-26 14:11:38 -0500

    Updated badge styles for more contrast with background colors

--- src/components/BadgePokemon.jsx
+++ src/components/BadgePokemon.jsx
@@ -7,14 +7,15 @@ export default function BadgePokemon({ types }) {
   return types.map((pk) => (
     <Badge
       key={pk}
-      fontSize="9"
+      fontSize="8"
+      variant="outline"
       borderRadius="full"
       px="2"
       py="0.5"
       mb="1"
       mr="1"
       backgroundColor={badgeTypeColor[pk]}
-      color="gray.700"
+      color="gray.100"
     >
       {pk}
     </Badge>
