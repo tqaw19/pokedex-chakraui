48fb5aeeb66a073d6d74b94782adc3397ba53a0b
tqaw19 <tqaw19@gmail.com>
2021-10-26 14:02:19 -0500

    Change return types to be compatible and reusable from different components.

--- src/components/BadgePokemon.jsx
+++ src/components/BadgePokemon.jsx
@@ -6,18 +6,17 @@ import { badgeTypeColor } from "../utils/badgeTypeColor";
 export default function BadgePokemon({ types }) {
   return types.map((pk) => (
     <Badge
-      key={pk.type.name}
-      // variant="outline"
+      key={pk}
       fontSize="9"
       borderRadius="full"
       px="2"
       py="0.5"
       mb="1"
       mr="1"
-      backgroundColor={badgeTypeColor[pk.type.name]}
+      backgroundColor={badgeTypeColor[pk]}
       color="gray.700"
     >
-      {pk.type.name}
+      {pk}
     </Badge>
   ));
 }
