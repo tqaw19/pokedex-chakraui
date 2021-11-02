1449067576a6483598c306232724d6feaa05e1b5
tqaw19 <tqaw19@gmail.com>
2021-11-02 17:21:29 -0500

    Added Weaknesses component and props values.

---
+++ src/components/WeaknessesPokemnon.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Box, Text } from "@chakra-ui/layout";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs";
+
+import {
+  multiplierDamageFrom,
+  multiplierDamageTo,
+} from "../utils/multiplierRelations";
+import BadgePokemon from "./BadgePokemon";
+
+export default function WeaknessesPokemnon(props) {
+  const { damage_relations } = props;
+
+  const [tabStat, setTabStat] = useState(multiplierDamageFrom);
+
+  const weaknesses = Object.entries(damage_relations ?? {})
+    .filter(([multiplier, value]) => {
+      if (value.length === 0) {
+        delete multiplierDamageFrom[multiplier];
+        delete multiplierDamageTo[multiplier];
+      } else {
+        return tabStat[multiplier];
+      }
+    })
+    .map(([multiplier, value]) => {
+      // console.log(multiplier);
+      // double_damage_from
+      // half_damage_from
+      // no_damage_from
+      return (
+        <div key={multiplier}>
+          {multiplierDamageFrom[multiplier] || multiplierDamageTo[multiplier]} -{" "}
+          <BadgePokemon types={value.map((pk) => pk.name)} />
+        </div>
+      );
+    });
+
+  return (
+    <Box mt="4">
+      <Tabs size="sm" variant="enclosed">
+        <Text mb="1">Multiplier</Text>
+        <TabList justifyContent="end">
+          <Tab onClick={() => setTabStat(multiplierDamageFrom)}>Weak</Tab>
+          <Tab onClick={() => setTabStat(multiplierDamageTo)}>Strong</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>{weaknesses}</TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
+}
