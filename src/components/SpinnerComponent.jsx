02d0361e58adb50c2f2caa88393321f6bf2a4c5b
tqaw19 <tqaw19@gmail.com>
2021-11-04 00:48:16 -0500

    Spinner now accepts dynamic props to change its color.

--- src/components/SpinnerComponent.jsx
+++ src/components/SpinnerComponent.jsx
@@ -2,11 +2,11 @@ import { Flex, Text } from "@chakra-ui/layout";
 import { Spinner } from "@chakra-ui/spinner";
 
 // Dumb spinner component to show when the page is laoding
-export default function SpinnerComponent() {
+export default function SpinnerComponent({ color = "black" }) {
   return (
     <Flex align="center" direction="column" m="36">
-      <Spinner />
-      <Text m="4" fontSize="smaller">
+      <Spinner color={color} />
+      <Text m="4" fontSize="smaller" color={color}>
         Loading
       </Text>
     </Flex>
