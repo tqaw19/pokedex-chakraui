07594a16273bc463f1aa7bf7f6fadef64b4103cf
tqaw19 <tqaw19@gmail.com>
2021-11-05 17:09:18 -0500

    Created component and divided stats data to show in columns.

---
+++ src/components/StatsPokemon.jsx
@@ -0,0 +1,36 @@
+import { Flex, Spacer, Text } from "@chakra-ui/layout";
+import React from "react";
+
+export default function StatsPokemon({ stats }) {
+  return (
+    <Flex
+      bg="blue.400"
+      mt="4"
+      borderRadius="lg"
+      fontSize="smaller"
+      color="white"
+    >
+      <Text as="span" m="4">
+        {stats.slice(0, 3).map((item, index) => (
+          <div key={`stat0012${index}`}>
+            <Text sx={{ textTransform: "capitalize" }}>{item.stat.name}</Text>{" "}
+            <Text mb="4" color="black">
+              {item["base_stat"]}
+            </Text>
+          </div>
+        ))}
+      </Text>
+      <Spacer />
+      <Text as="span" m="4" mr={["6", null, null, "12"]}>
+        {stats.slice(3).map((item, index) => (
+          <div key={`stat001${index}`}>
+            <Text sx={{ textTransform: "capitalize" }}>{item.stat.name}</Text>{" "}
+            <Text mb="4" color="black">
+              {item["base_stat"]}
+            </Text>
+          </div>
+        ))}
+      </Text>
+    </Flex>
+  );
+}
