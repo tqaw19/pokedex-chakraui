b3e0b42ca6dcf2193db31d745d8cc01f715ff7fb
tqaw19 <tqaw19@gmail.com>
2021-11-12 14:47:11 -0500

    Added mocking evolutions images inside component

--- src/components/EvolutionsComponent.jsx
+++ src/components/EvolutionsComponent.jsx
@@ -1,5 +1,16 @@
+import { Image } from "@chakra-ui/image";
+import { Flex, Text } from "@chakra-ui/layout";
 import React from "react";
 
-export default function EvolutionsComponent() {
-  return <div></div>;
+export default function EvolutionsComponent({ pokemonImage, name }) {
+  return (
+    <>
+      <Text>Evolution Chain</Text>
+      <Flex direction="row" justify="space-between" my="4">
+        <Image src={pokemonImage} alt={name} boxSize="100px" mx="auto" />
+        <Image src={pokemonImage} boxSize="100px" alt={name} mx="auto" />
+        <Image src={pokemonImage} boxSize="100px" alt={name} mx="auto" />
+      </Flex>
+    </>
+  );
 }
