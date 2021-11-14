498d6544db99cbc92999a40799fe5c525773396a
tqaw19 <tqaw19@gmail.com>
2021-11-14 16:55:17 -0500

    Updated title inside stats, they're more consisntent and uppercase.

--- src/components/StatsPokemon.jsx
+++ src/components/StatsPokemon.jsx
@@ -15,18 +15,24 @@ export default function StatsPokemon({ stats }) {
         <Text as="span" m="4">
           {stats.slice(0, 3).map((item, index) => (
             <div key={`stat0012${index}`}>
-              <Text sx={{ textTransform: "capitalize" }}>{item.stat.name}</Text>{" "}
+              <Text fontSize="xs" sx={{ textTransform: "uppercase" }}>
+                {item.stat.name}
+              </Text>{" "}
               <Text mb="4" color="black">
                 {item["base_stat"]}
               </Text>
             </div>
           ))}
         </Text>
+
         <Spacer />
+
         <Text as="span" m="4" mr={["6", null, null, "12"]}>
           {stats.slice(3).map((item, index) => (
             <div key={`stat001${index}`}>
-              <Text sx={{ textTransform: "capitalize" }}>{item.stat.name}</Text>{" "}
+              <Text fontSize="xs" sx={{ textTransform: "uppercase" }}>
+                {item.stat.name}
+              </Text>{" "}
               <Text mb="4" color="black">
                 {item["base_stat"]}
               </Text>
