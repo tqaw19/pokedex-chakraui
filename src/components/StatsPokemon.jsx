6c1f8634e84b4d89557af957a1bcd74e146e2ea6
tqaw19 <tqaw19@gmail.com>
2021-11-14 16:49:59 -0500

    Added title inside baseStats component so it's more descriptive about the containing information.

--- src/components/StatsPokemon.jsx
+++ src/components/StatsPokemon.jsx
@@ -3,34 +3,37 @@ import React from "react";
 
 export default function StatsPokemon({ stats }) {
   return (
-    <Flex
-      bg="blue.400"
-      mt="4"
-      borderRadius="lg"
-      fontSize="smaller"
-      color="white"
-    >
-      <Text as="span" m="4">
-        {stats.slice(0, 3).map((item, index) => (
-          <div key={`stat0012${index}`}>
-            <Text sx={{ textTransform: "capitalize" }}>{item.stat.name}</Text>{" "}
-            <Text mb="4" color="black">
-              {item["base_stat"]}
-            </Text>
-          </div>
-        ))}
-      </Text>
-      <Spacer />
-      <Text as="span" m="4" mr={["6", null, null, "12"]}>
-        {stats.slice(3).map((item, index) => (
-          <div key={`stat001${index}`}>
-            <Text sx={{ textTransform: "capitalize" }}>{item.stat.name}</Text>{" "}
-            <Text mb="4" color="black">
-              {item["base_stat"]}
-            </Text>
-          </div>
-        ))}
-      </Text>
-    </Flex>
+    <>
+      <Text mt="6">Base Stats</Text>
+      <Flex
+        bg="blue.400"
+        mt="2"
+        borderRadius="lg"
+        fontSize="smaller"
+        color="white"
+      >
+        <Text as="span" m="4">
+          {stats.slice(0, 3).map((item, index) => (
+            <div key={`stat0012${index}`}>
+              <Text sx={{ textTransform: "capitalize" }}>{item.stat.name}</Text>{" "}
+              <Text mb="4" color="black">
+                {item["base_stat"]}
+              </Text>
+            </div>
+          ))}
+        </Text>
+        <Spacer />
+        <Text as="span" m="4" mr={["6", null, null, "12"]}>
+          {stats.slice(3).map((item, index) => (
+            <div key={`stat001${index}`}>
+              <Text sx={{ textTransform: "capitalize" }}>{item.stat.name}</Text>{" "}
+              <Text mb="4" color="black">
+                {item["base_stat"]}
+              </Text>
+            </div>
+          ))}
+        </Text>
+      </Flex>
+    </>
   );
 }
